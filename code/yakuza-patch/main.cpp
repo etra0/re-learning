f9a62f417dc815e36c8b87ef2e7719b02c615d5e
Sebastián Aedo <saedo@alumnos.inf.utfsm.cl>
2020-04-18 01:29:10 -0400

    updated readme, added ifdef for kiwami

--- code/yakuza-patch/main.cpp
+++ code/yakuza-patch/main.cpp
@@ -5,7 +5,11 @@ void OnInitializeHook() {
     using namespace Memory::VP;
     using namespace hook;
 
+    #ifndef KIWAMI
     auto sprint = get_pattern("F3 0F 11 4B 0C E9 CD", 0);
+    #else
+    auto sprint = get_pattern("F3 0F 11 43 0C E9 D3", 0);
+    #endif
     Nop(sprint, 5);
 
 
